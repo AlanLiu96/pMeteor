a98b518010231a167e51f06f5f9f7de2baa30a1f
Alan Liu <AlanL.Liu@yahoo.com>
2014-11-07 17:39:01 -0500

    resolved double item bug in yms and took out greeting template

--- homepage/personalmeteor.js
+++ homepage/personalmeteor.js
@@ -39,18 +39,18 @@ Template.fof.showfof= function(){
 } // shows fof routing
 
 
-  Template.hello.greeting = function () {
-    return "Welcome to personalmeteor.";
-  };
+  // Template.hello.greeting = function () {
+  //   return "Welcome to personalmeteor.";
+  // };
 
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  // Template.hello.events({
+  //   'click input': function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button");
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
